e542dfac291f5bc71cd362ea11126927b8aa2bc5
Daniel <daniel.mileski@cidadania.gov.br>
2021-09-27 23:30:17 -0300

    Alterando o estado do redux

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,39 @@
+import { useCallback, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import api from "../services/api";
+import { addProductToCart } from "../store/modules/cart/actions";
+import { IProduct } from "../store/modules/cart/types";
+
+export function Catalog() {
+  const dispatch = useDispatch();
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    api.get("products").then((response) => {
+      setCatalog(response.data);
+    });
+  }, []);
+
+  const handleAddProductToCart = useCallback(
+    (product) => {
+      dispatch(addProductToCart(product));
+    },
+    [dispatch]
+  );
+
+  return (
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map((product) => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <span>{product.price}</span> {" - "}
+          <button type="button" onClick={() => handleAddProductToCart(product)}>
+            Comprar
+          </button>
+        </article>
+      ))}
+    </main>
+  );
+}
